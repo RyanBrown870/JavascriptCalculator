e1d14df237342ca7008f72d05a20986115379033
rxb870 <ryanbrown0689@gmail.com>
2020-11-15 10:54:51 +0000

    redundant files deleted

--- src/components/AC.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function AC(props) {
-    return (
-        <div>
-            <button type="button" className="btn btn-block btn-danger" onClick={props.handleClick}>{props.item}</button>
-        </div>
-    )
-}
